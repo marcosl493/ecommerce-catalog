9eecdc4de44340fad3bb9922c9d0cc183c488929
Marcos Lima <marcosviniciuslima0610@gmail.com>
2026-02-10 20:51:33 -0300

    chore: cria configuracao de conexao com o banco de dados

---
+++ src/Infrastructure/Persistence/CatalogDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Persistence;
+
+public class CatalogDbContext
+    (
+        DbContextOptions<CatalogDbContext> options
+    ) : DbContext(options)
+{
+}
