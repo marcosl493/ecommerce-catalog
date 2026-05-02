e97438ef0802e97a532e1d361ea32281fd1831a7
Marcos Vinícius Lima <marcosviniciuslima0610@gmail.com>
2026-05-02 20:08:42 -0300

    Observabilidade com ELK e APM (#4)
    * chore: adiciona no gitignore a pasta .vscode
    
    * feat: adiciona suporte a logs na stack elk, para webapi e database
    
    * feat: adiciona configuração do APM Server e suporte a logs no Elasticsearch
    
    * feat: atualiza configuração do Serilog para integração com Elasticsearch e ajusta variáveis de ambiente
    
    * feat: atualiza tokens de autenticação no docker-compose para variáveis de ambiente
    
    * feat: remove configuração do Serilog do arquivo appsettings.Staging.json
    
    ---------

--- src/WebApi/Program.ServiceInjections.cs
+++ src/WebApi/Program.ServiceInjections.cs
@@ -1,14 +1,46 @@
-﻿using Serilog;
+﻿using Elastic.Channels;
+using Elastic.Ingest.Elasticsearch;
+using Elastic.Ingest.Elasticsearch.DataStreams;
+using Elastic.Serilog.Sinks;
+using Elastic.Transport;
+using Serilog;
+using Serilog.Events;
 
 namespace WebApi;
 
 public static partial class Program
 {
-    public static IServiceCollection AddSerilog(this IServiceCollection services)
+    public static IServiceCollection AddSerilog(this IServiceCollection services, IConfiguration configuration)
     {
-        services.AddSerilog((services, lc) => lc
-                .ReadFrom.Configuration(services.GetRequiredService<IConfiguration>())
-                .ReadFrom.Services(services));
+        var elasticUrl = configuration["ELASTICSEARCH_URL"]!;
+        var elasticUsername = configuration["ELASTICSEARCH_USERNAME"]!;
+        var elasticPassword = configuration["ELASTICSEARCH_PASSWORD"]!;
+        var indexName = configuration["ELASTICSEARCH_INDEX_NAME"]!;
+        
+       Log.Logger = new LoggerConfiguration()
+	    .MinimumLevel.Debug()
+	    .Enrich.FromLogContext()
+        .WriteTo.Console()
+        .WriteTo.Elasticsearch([new Uri(elasticUrl)], opts =>
+{
+    opts.MinimumLevel = LogEventLevel.Information;
+	opts.DataStream = new DataStreamName("logs", indexName, "default");
+	opts.BootstrapMethod = BootstrapMethod.None;
+	opts.ConfigureChannel = channelOpts =>
+	{
+        channelOpts.BufferOptions = new BufferOptions
+        {
+            InboundBufferMaxSize = 100_000,
+            OutboundBufferMaxSize = 1_000,
+            OutboundBufferMaxLifetime = TimeSpan.FromSeconds(5),
+            ExportMaxConcurrency = null
+	};};
+}, transport =>
+{
+	 transport.Authentication(new BasicAuthentication(elasticUsername, elasticPassword));
+})
+        .CreateLogger();
+        
         return services;
     }
 }
